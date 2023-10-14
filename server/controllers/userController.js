b3235b1f62002944962ccae13101c8e4381dcd2d
chethanavirajini <cheth.virajini@gmail.com>
2023-10-14 12:36:59 +0530

    user login state update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,21 @@
 var connection = require('../db/db-connection');
+const bcrypt = require('bcrypt')
 
-const userSignUp = (req, res)=>{
+
+const userSignUp = async(req, res)=>{
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        connection.query('insert into usersignup(username, email, password) values (?,?,?);', [req.body.username, req.body.email, hashedPassword], (err, result)=>{
+            if(err) throw err
     
-    connection.query('insert into usersignup(username, email, password) values (?,?,?);', [req.body.username, req.body.email, req.body.password], (err, result)=>{
-        if(err) throw err
-
-        res.status(200).json(result)
-    })
+            res.status(200).json(result)
+        })
+    }catch(err){
+        res.status(500).send(err)
+    }
+   
+    
+    
 }
 
 module.exports = {userSignUp};
--- user_authetication/src/pages/userLogin.jsx
+++ user_authetication/src/pages/userLogin.jsx
@@ -1,20 +1,45 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
+import { useState } from 'react';
+import axios from 'axios'
 
 export default function Login(){
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(username==="" && password===""){
+            alert("All fileds are required!!")
+        }else{
+            axios.post('http://localhost:3000/api/login',{
+                username: username,
+                password: password
+            })
+            .then((response)=>{
+                console.log(response.data)
+            })
+            .catch((Error)=>{
+                console.log(Error)
+            })
+        }
+    }
+ 
+
+
     return(
         <>
-            <Form className='w-50 m-auto mt-5'>
+            <Form onSubmit={handleSubmit} className='w-50 m-auto mt-5'>
                 <h3 className='text-center'>User Login</h3>
                 <Form.Group className="mb-3">
                     <Form.Label>User Name </Form.Label>
-                    <Form.Control type="text" placeholder="Enter Name" />
+                    <Form.Control onChange={(e)=>{setUsername(e.target.value)}} type="text" placeholder="Enter Name" />
                 </Form.Group>
                 
                 <Form.Group className="mb-3" controlId="formGroupPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder="Password" />
                 </Form.Group>
                 <Button className='w-100 mt-3' variant="primary" type="submit">
                     Submit
