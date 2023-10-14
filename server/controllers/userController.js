c3b3dc6e41f8ecd3fe39b749264a620672abe71b
chethanavirajini <cheth.virajini@gmail.com>
2023-10-14 12:58:41 +0530

    genrate token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 var connection = require('../db/db-connection');
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
 
 const userSignUp = async(req, res)=>{
     try{
@@ -12,10 +12,28 @@ const userSignUp = async(req, res)=>{
         })
     }catch(err){
         res.status(500).send(err)
-    }
-   
-    
-    
+    } 
 }
 
-module.exports = {userSignUp};
+
+const userLogin = async(req, res)=>{
+    const {username, password} = req.body;
+    connection.query('select * from usersignup where username=?;',[username],(err, result)=>{
+        console.log(result)
+        if(!result.length){
+            res.json({Error:"User doesn't exist please sign up first!"})
+            return;
+        }
+        bcrypt.compare(password, result[0].password).then(async(match)=>{
+            if(!match){
+                res.json({Error:"Wrong username and password!"})
+            }else{
+                const token = jwt.sign({username:result[0].username, id:result[0].user_id}, "jwt")
+                console.log(token)
+            }
+        })
+
+    })
+}
+
+module.exports = {userSignUp, userLogin};
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {userSignUp} = require('../controllers/userController');
+const {userSignUp, userLogin} = require('../controllers/userController');
 
 
 router.post('/signup', userSignUp);
+router.post('/login', userLogin);
 
 module.exports = router;
